d9887f4b9aa216cf2e5a5f0305cddc12d58fc888
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2022-01-30 10:57:19 -0500

    Update trie.c

--- prefix_trie/trie.c
+++ prefix_trie/trie.c
@@ -10,8 +10,28 @@ struct Node {
 Node node_pool[Node_pool_cap];
 size_t node_pool_count = 0;
 
-int main()
+Node *alloc_node (void) // simple allocator function
 {
-	// TODO
+	assert(node_pool_count < Node_pool_cap);
+	return &node_pool[node_pool_count++];
+}
+
+void insert_text (Node *root, const char *text)
+{
+	if (*text) return;
+
+	assert(text != NULL);
+
+	size_t index = (size_t) *text;
+	if (root->children[index] == NULL )
+		root->children[index] = alloc_node();
+
+	insert_text(root->children[index], text + 1);
+}
+
+int main(void)
+{
+	Node *root = alloc_node();
+	insert_text(root, "hello");
 	return 0;
 }
